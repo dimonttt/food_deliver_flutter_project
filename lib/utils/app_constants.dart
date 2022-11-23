1782f4ce1375e07756a26941af93700e590f3a6c
Olya Dimont <olga.dimont@yandex.ru>
2022-11-23 14:21:25 +0300

    загрузка данных с сайта

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -2,8 +2,9 @@ class AppConstans {
   static const String APP_NAME = "DBFood";
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL = "http://mvs.bslmeiyu.com";
+  static const String BASE_URL = "http://127.0.0.1:8000";
   //static const String BASE_URL = "http://10.0.2.2:8000";
+  //static const String BASE_URL = "http://mvs.bslmeiyu.com";
 
   static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
   static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
@@ -11,7 +12,5 @@ class AppConstans {
 
   static const String TOKEN = "DBtoken";
   static const String CART_LIST = "cart-list";
-    static const String CART_HISTORY_LIST = "cart-history-list";
-
-
+  static const String CART_HISTORY_LIST = "cart-history-list";
 }
